4325f312fbabf0f20463dcaf3df0c4cdc2752d8f
Fabien Batteix <skywodd@gmail.com>
2016-02-02 17:37:49 +0100

    Made render API flexible by using **kwargs instead of hardcoded keyword arguments. Also updated docstring.

--- skcode/render.py
+++ skcode/render.py
@@ -3,93 +3,97 @@ SkCode rendering code.
 """
 
 
-def render_inner_html(tree_node, force_rel_nofollow=True):
+def render_inner_html(tree_node, **kwargs):
     """
-    Render all children of the given AST tree as HTML.
+    Render all children of the given tree node as HTML.
     :param tree_node: The parent tree node with children to be rendered.
-    :param force_rel_nofollow: If set, all links in the rendered HTML will have "rel=nofollow" (default to True).
+    :param kwargs: Extra keywords arguments for the ``render_html`` callback method.
     :return The rendered children tree as HTML.
     """
 
-    # Get inner HTML
+    # Get the inner HTML
     inner_html = []
     for child_node in tree_node.children:
-        inner_html.append(render_to_html(child_node, force_rel_nofollow))
+        inner_html.append(render_to_html(child_node, **kwargs))
 
     # Return the inner HTML as string
     return ''.join(inner_html)
 
 
-def render_to_html(tree_node, force_rel_nofollow=True):
+def render_to_html(tree_node, **kwargs):
     """
-    Render the given AST tree as HTML.
-    :param tree_node: The document tree node to be rendered.
-    :param force_rel_nofollow: If set, all links in the rendered HTML will have "rel=nofollow" (default to True).
+    Render the given tree node and children recursively as HTML.
+    :param tree_node: The tree node to be rendered.
+    :param kwargs: Extra keywords arguments for the ``render_html`` callback method.
     :return The rendered document tree as HTML.
     """
 
-    # Get inner HTML
-    inner_html = render_inner_html(tree_node, force_rel_nofollow)
+    # Get the inner HTML
+    inner_html = render_inner_html(tree_node, **kwargs)
 
     # Render the node
-    return tree_node.opts.render_html(tree_node, inner_html, force_rel_nofollow)
+    return tree_node.opts.render_html(tree_node, inner_html, **kwargs)
 
 
-def render_inner_text(tree_node):
+def render_inner_text(tree_node, **kwargs):
     """
-    Render all children of the given AST tree as text.
+    Render all children of the given tree node as text.
     :param tree_node: The parent tree node with children to be rendered.
+    :param kwargs: Extra keywords arguments for the ``render_text`` callback method.
     :return The rendered children tree as text.
     """
 
-    # Get inner text
+    # Get the inner text
     inner_text = []
     for child_node in tree_node.children:
-        inner_text.append(render_to_text(child_node))
+        inner_text.append(render_to_text(child_node, **kwargs))
 
     # Return the inner text as string
     return ''.join(inner_text)
 
 
-def render_to_text(tree_node):
+def render_to_text(tree_node, **kwargs):
     """
-    Render the given AST tree as text.
-    :param tree_node: The document tree node to be rendered.
+    Render the given tree node and children recursively as text.
+    :param tree_node: The tree node to be rendered.
+    :param kwargs: Extra keywords arguments for the ``render_text`` callback method.
     :return The rendered document tree as text.
     """
 
-    # Get inner text
-    inner_text = render_inner_text(tree_node)
+    # Get the inner text
+    inner_text = render_inner_text(tree_node, **kwargs)
 
     # Render the node
-    return tree_node.opts.render_text(tree_node, inner_text)
+    return tree_node.opts.render_text(tree_node, inner_text, **kwargs)
 
 
-def render_inner_skcode(tree_node):
+def render_inner_skcode(tree_node, **kwargs):
     """
-    Render all children the given AST tree as SkCode.
+    Render all children the given tree node as SkCode.
     :param tree_node: The parent tree node with children to be rendered.
+    :param kwargs: Extra keywords arguments for the ``render_skcode`` callback method.
     :return The rendered children tree as SkCode.
     """
 
-    # Get inner skcode
+    # Get the inner SkCode
     inner_skcode = []
     for child_node in tree_node.children:
-        inner_skcode.append(render_to_skcode(child_node))
+        inner_skcode.append(render_to_skcode(child_node, **kwargs))
 
-    # Return the inner skcode as string
+    # Return the inner SkCode as string
     return ''.join(inner_skcode)
 
 
-def render_to_skcode(tree_node):
+def render_to_skcode(tree_node, **kwargs):
     """
-    Render the given AST tree as SkCode.
+    Render the given tree node and children recursively as SkCode.
     :param tree_node: The document tree node to be rendered.
+    :param kwargs: Extra keywords arguments for the ``render_skcode`` callback method.
     :return The rendered document tree as SkCode.
     """
 
-    # Get inner skcode
-    inner_skcode = render_inner_skcode(tree_node)
+    # Get inner SkCode
+    inner_skcode = render_inner_skcode(tree_node, **kwargs)
 
     # Render the node
-    return tree_node.opts.render_skcode(tree_node, inner_skcode)
+    return tree_node.opts.render_skcode(tree_node, inner_skcode, **kwargs)
