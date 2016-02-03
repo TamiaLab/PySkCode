381f4d270ccc7a940c005fd4a4ac181a6fe79df4
Fabien Batteix <skywodd@gmail.com>
2016-02-03 16:23:29 +0100

    Updated tests suite

--- tests/tests_tags/tests_textdirection.py
+++ tests/tests_tags/tests_textdirection.py
@@ -4,8 +4,9 @@ SkCode text direction tags test code.
 
 import unittest
 
-from skcode.etree import TreeNode
-from skcode.tags import (DirectionTextTagOptions,
+from skcode.etree import RootTreeNode
+from skcode.tags import (RootTagOptions,
+                         DirectionTextTagOptions,
                          FixedDirectionTextTagOptions,
                          TEXT_DIR_LEFT_TO_RIGHT,
                          TEXT_DIR_RIGHT_TO_LEFT,
@@ -14,6 +15,11 @@ from skcode.tags import (DirectionTextTagOptions,
 
 class DirectionTextTagTestCase(unittest.TestCase):
     """ Tests suite for the text direction tag module. """
+
+    def test_module_constants(self):
+        """ Test module level constants """
+        self.assertEqual(TEXT_DIR_LEFT_TO_RIGHT, 1)
+        self.assertEqual(TEXT_DIR_RIGHT_TO_LEFT, 2)
 
     def test_tag_and_aliases_in_default_recognized_tags_dict(self):
         """ Test the presence of the tag and aliases in the dictionary of default recognized tags. """
@@ -50,42 +56,56 @@ class DirectionTextTagTestCase(unittest.TestCase):
     def test_get_text_direction_with_tagname_set(self):
         """ Test the ``get_text_direction`` with the tag name attribute set. """
         opts = DirectionTextTagOptions()
-        tree_node = TreeNode(None, 'bdo', opts, attrs={'bdo': 'rtl'})
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('bdo', opts, attrs={'bdo': 'rtl'})
         text_direction = opts.get_text_direction(tree_node)
         self.assertEqual(TEXT_DIR_RIGHT_TO_LEFT, text_direction)
 
     def test_get_text_direction_with_dir_attr_set(self):
         """ Test the ``get_text_direction`` with the "dir" attribute set. """
         opts = DirectionTextTagOptions()
-        tree_node = TreeNode(None, 'bdo', opts, attrs={'dir': 'rtl'})
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('bdo', opts, attrs={'dir': 'rtl'})
         text_direction = opts.get_text_direction(tree_node)
         self.assertEqual(TEXT_DIR_RIGHT_TO_LEFT, text_direction)
 
     def test_get_text_direction_with_tagname_and_dir_attr_set(self):
         """ Test the ``get_text_direction`` with the tag name and "dir" attribute  set. """
         opts = DirectionTextTagOptions()
-        tree_node = TreeNode(None, 'bdo', opts, attrs={'dir': 'rtl', 'bdo': 'ltr'})
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('bdo', opts, attrs={'dir': 'rtl', 'bdo': 'ltr'})
         text_direction = opts.get_text_direction(tree_node)
         self.assertEqual(TEXT_DIR_LEFT_TO_RIGHT, text_direction)
 
     def test_get_text_direction_with_default_value(self):
         """ Test the ``get_text_direction`` without any value set. """
         opts = DirectionTextTagOptions()
-        tree_node = TreeNode(None, 'bdo', opts, attrs={})
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('bdo', opts, attrs={})
         text_direction = opts.get_text_direction(tree_node)
         self.assertEqual(opts.default_text_direction, text_direction)
+
+    def test_get_text_direction_with_uppercase(self):
+        """ Test the ``get_text_direction`` with the "dir" attribute set. """
+        opts = DirectionTextTagOptions()
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('bdo', opts, attrs={'dir': 'RtL'})
+        text_direction = opts.get_text_direction(tree_node)
+        self.assertEqual(TEXT_DIR_RIGHT_TO_LEFT, text_direction)
 
     def test_get_text_direction_with_invalid_value(self):
         """ Test the ``get_text_direction`` with an invalid/unknown value set. """
         opts = DirectionTextTagOptions()
-        tree_node = TreeNode(None, 'bdo', opts, attrs={'dir': 'foobar'})
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('bdo', opts, attrs={'dir': 'foobar'})
         text_direction = opts.get_text_direction(tree_node)
         self.assertEqual(opts.default_text_direction, text_direction)
 
     def test_render_html_ltr(self):
         """ Test the ``render_html`` method in LTR mode. """
         opts = DirectionTextTagOptions()
-        tree_node = TreeNode(None, 'bdo', opts, attrs={'dir': 'ltr'})
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('bdo', opts, attrs={'dir': 'ltr'})
         output_result = opts.render_html(tree_node, 'john doe')
         expected_result = '<bdo dir="ltr">john doe</bdo>'
         self.assertEqual(expected_result, output_result)
@@ -93,7 +113,8 @@ class DirectionTextTagTestCase(unittest.TestCase):
     def test_render_html_rtl(self):
         """ Test the ``render_html`` method in RTL mode. """
         opts = DirectionTextTagOptions()
-        tree_node = TreeNode(None, 'bdo', opts, attrs={'dir': 'rtl'})
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('bdo', opts, attrs={'dir': 'rtl'})
         output_result = opts.render_html(tree_node, 'john doe')
         expected_result = '<bdo dir="rtl">john doe</bdo>'
         self.assertEqual(expected_result, output_result)
@@ -101,7 +122,8 @@ class DirectionTextTagTestCase(unittest.TestCase):
     def test_render_text_ltr(self):
         """ Test the ``render_text`` method in LTR mode. """
         opts = DirectionTextTagOptions()
-        tree_node = TreeNode(None, 'bdo', opts, attrs={'dir': 'ltr'})
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('bdo', opts, attrs={'dir': 'ltr'})
         output_result = opts.render_text(tree_node, 'john doe')
         expected_result = 'john doe'
         self.assertEqual(expected_result, output_result)
@@ -109,30 +131,34 @@ class DirectionTextTagTestCase(unittest.TestCase):
     def test_render_text_rtl(self):
         """ Test the ``render_text`` method in RTL mode. """
         opts = DirectionTextTagOptions()
-        tree_node = TreeNode(None, 'bdo', opts, attrs={'dir': 'rtl'})
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('bdo', opts, attrs={'dir': 'rtl'})
         output_result = opts.render_text(tree_node, 'john doe')
         expected_result = 'eod nhoj'
         self.assertEqual(expected_result, output_result)
 
-    def test_render_skcode_ltr(self):
-        """ Test the ``render_skcode`` method in LTR mode. """
+    def test_get_skcode_attributes(self):
+        """ Test the ``get_skcode_attributes`` method. """
         opts = DirectionTextTagOptions()
-        tree_node = TreeNode(None, 'bdo', opts, attrs={'dir': 'ltr'})
-        output_result = opts.render_skcode(tree_node, 'john doe')
-        expected_result = '[bdo dir="ltr"]john doe[/bdo]'
-        self.assertEqual(expected_result, output_result)
-
-    def test_render_skcode_rtl(self):
-        """ Test the ``render_skcode`` method in RTL mode. """
-        opts = DirectionTextTagOptions()
-        tree_node = TreeNode(None, 'bdo', opts, attrs={'dir': 'rtl'})
-        output_result = opts.render_skcode(tree_node, 'john doe')
-        expected_result = '[bdo dir="rtl"]john doe[/bdo]'
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('bdo', opts, attrs={'dir': 'ltr'})
+        output_result = opts.get_skcode_attributes(tree_node, 'john doe')
+        expected_result = ({'dir': 'ltr'}, 'dir')
         self.assertEqual(expected_result, output_result)
 
 
-class FixedDirectionTextTagtestCase(unittest.TestCase):
+class FixedDirectionTextTagTestCase(unittest.TestCase):
     """ Tests suite for the fixed text direction tag module. """
+
+    def test_subclassing(self):
+        """ Test super class """
+        self.assertTrue(issubclass(FixedDirectionTextTagOptions, DirectionTextTagOptions))
+
+    def test_assertion_constructor(self):
+        """ Test assertion at ``__init__`` """
+        with self.assertRaises(AssertionError) as e:
+            FixedDirectionTextTagOptions(None)
+        self.assertEqual('The text direction is mandatory.', str(e.exception))
 
     def test_tag_and_aliases_in_default_recognized_tags_dict(self):
         """ Test the presence of the tag and aliases in the dictionary of default recognized tags. """
@@ -145,23 +171,17 @@ class FixedDirectionTextTagtestCase(unittest.TestCase):
 
     def test_get_text_direction(self):
         """ Test the ``get_text_direction``. """
-        opts = FixedDirectionTextTagOptions(2)
-        tree_node = TreeNode(None, 'bdo', opts, attrs={})
-        text_direction = opts.get_text_direction(tree_node)
-        self.assertEqual(2, text_direction)
-
-    def test_render_skcode_ltr(self):
-        """ Test the ``render_skcode`` method in LTR mode. """
-        opts = FixedDirectionTextTagOptions(TEXT_DIR_LEFT_TO_RIGHT)
-        tree_node = TreeNode(None, 'ltr', opts, attrs={})
-        output_result = opts.render_skcode(tree_node, 'john doe')
-        expected_result = '[ltr]john doe[/ltr]'
-        self.assertEqual(expected_result, output_result)
-
-    def test_render_skcode_rtl(self):
-        """ Test the ``render_skcode`` method in RTL mode. """
         opts = FixedDirectionTextTagOptions(TEXT_DIR_RIGHT_TO_LEFT)
-        tree_node = TreeNode(None, 'rtl', opts, attrs={})
-        output_result = opts.render_skcode(tree_node, 'john doe')
-        expected_result = '[rtl]john doe[/rtl]'
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('bdo', opts, attrs={'dir': 'test'})
+        text_direction = opts.get_text_direction(tree_node)
+        self.assertEqual(TEXT_DIR_RIGHT_TO_LEFT, text_direction)
+
+    def test_get_skcode_attributes(self):
+        """ Test the ``get_skcode_attributes`` method. """
+        opts = FixedDirectionTextTagOptions(TEXT_DIR_RIGHT_TO_LEFT)
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('bdo', opts, attrs={})
+        output_result = opts.get_skcode_attributes(tree_node, 'john doe')
+        expected_result = ({}, None)
         self.assertEqual(expected_result, output_result)
