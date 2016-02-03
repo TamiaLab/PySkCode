a664c255050cb03b3b6c6f2a3e254806cd595db1
Fabien Batteix <skywodd@gmail.com>
2016-02-03 15:48:56 +0100

    Updated tests suite

--- tests/tests_tags/tests_tables.py
+++ tests/tests_tags/tests_tables.py
@@ -4,15 +4,16 @@ SkCode definitions tag test code.
 
 import unittest
 
-from skcode.etree import TreeNode
-from skcode.tags import (TableTagOptions,
+from skcode.etree import RootTreeNode
+from skcode.tags import (RootTagOptions,
+                         TableTagOptions,
                          TableRowTagOptions,
                          TableCellTagOptions,
                          TableHeaderCellTagOptions,
                          DEFAULT_RECOGNIZED_TAGS)
 
 
-class TablesTagtestCase(unittest.TestCase):
+class TablesTagTestCase(unittest.TestCase):
     """ Tests suite for the tables tag module. """
 
     def test_tag_and_aliases_in_default_recognized_tags_dict(self):
@@ -31,33 +32,37 @@ class TablesTagtestCase(unittest.TestCase):
         self.assertFalse(opts.inline)
         self.assertTrue(opts.close_inlines)
         self.assertFalse(opts.make_paragraphs_here)
+        self.assertEqual('table table-condensed table-striped', opts.css_class_name)
 
     def test_html_rendering(self):
         """ Test HTML rendering. """
         opts = TableTagOptions()
-        tree_node = TreeNode(None, 'table', opts)
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('table', opts)
         rendered_output = opts.render_html(tree_node, 'test')
         expected_output = '<table class="table table-condensed table-striped">test</table>\n'
+        self.assertEqual(expected_output, rendered_output)
+
+    def test_html_rendering_custom_css(self):
+        """ Test HTML rendering with a custom CSS class name. """
+        opts = TableTagOptions(css_class_name='custom_css')
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('table', opts)
+        rendered_output = opts.render_html(tree_node, 'test')
+        expected_output = '<table class="custom_css">test</table>\n'
         self.assertEqual(expected_output, rendered_output)
 
     def test_text_rendering(self):
         """ Test text rendering. """
         opts = TableTagOptions()
-        tree_node = TreeNode(None, 'table', opts)
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('table', opts)
         rendered_output = opts.render_text(tree_node, 'test')
         expected_output = 'test'
         self.assertEqual(expected_output, rendered_output)
 
-    def test_skcode_rendering(self):
-        """ Test SkCode rendering. """
-        opts = TableTagOptions()
-        tree_node = TreeNode(None, 'table', opts)
-        rendered_output = opts.render_skcode(tree_node, 'test')
-        expected_output = '[table]test[/table]'
-        self.assertEqual(expected_output, rendered_output)
 
-
-class TableRowsTagtestCase(unittest.TestCase):
+class TableRowsTagTestCase(unittest.TestCase):
     """ Tests suite for the table rows tag module. """
 
     def test_tag_and_aliases_in_default_recognized_tags_dict(self):
@@ -80,7 +85,8 @@ class TableRowsTagtestCase(unittest.TestCase):
     def test_html_rendering(self):
         """ Test HTML rendering. """
         opts = TableRowTagOptions()
-        tree_node = TreeNode(None, 'tr', opts)
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('tr', opts)
         rendered_output = opts.render_html(tree_node, 'test')
         expected_output = '<tr>test</tr>\n'
         self.assertEqual(expected_output, rendered_output)
@@ -88,21 +94,14 @@ class TableRowsTagtestCase(unittest.TestCase):
     def test_text_rendering(self):
         """ Test text rendering. """
         opts = TableRowTagOptions()
-        tree_node = TreeNode(None, 'tr', opts)
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('tr', opts)
         rendered_output = opts.render_text(tree_node, 'test')
         expected_output = 'test'
         self.assertEqual(expected_output, rendered_output)
 
-    def test_skcode_rendering(self):
-        """ Test SkCode rendering. """
-        opts = TableRowTagOptions()
-        tree_node = TreeNode(None, 'tr', opts)
-        rendered_output = opts.render_skcode(tree_node, 'test')
-        expected_output = '[tr]test[/tr]'
-        self.assertEqual(expected_output, rendered_output)
 
-
-class TableCellsTagtestCase(unittest.TestCase):
+class TableCellsTagTestCase(unittest.TestCase):
     """ Tests suite for the table cells tag module. """
 
     def test_tag_and_aliases_in_default_recognized_tags_dict(self):
@@ -127,119 +126,104 @@ class TableCellsTagtestCase(unittest.TestCase):
     def test_get_cell_colspan(self):
         """ Test the ``get_cell_colspan`` with a valid value.  """
         opts = TableCellTagOptions()
-        tree_node = TreeNode(None, 'td', opts, attrs={'colspan': '3'})
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('td', opts, attrs={'colspan': '3'})
         colspan = opts.get_cell_colspan(tree_node)
         self.assertEqual(colspan, 3)
 
     def test_get_cell_colspan_with_no_value_set(self):
         """ Test the ``get_cell_colspan`` with no value set.  """
         opts = TableCellTagOptions()
-        tree_node = TreeNode(None, 'td', opts, attrs={})
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('td', opts, attrs={})
         colspan = opts.get_cell_colspan(tree_node)
         self.assertEqual(colspan, 1)
 
     def test_get_cell_colspan_with_non_number(self):
         """ Test the ``get_cell_colspan`` with an invalid non-number value.  """
         opts = TableCellTagOptions()
-        tree_node = TreeNode(None, 'td', opts, attrs={'colspan': 'abc'})
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('td', opts, attrs={'colspan': 'abc'})
         colspan = opts.get_cell_colspan(tree_node)
         self.assertEqual(colspan, 1)
 
     def test_get_cell_colspan_with_negative_value(self):
         """ Test the ``get_cell_colspan`` with an invalid negative value.  """
         opts = TableCellTagOptions()
-        tree_node = TreeNode(None, 'td', opts, attrs={'colspan': '-3'})
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('td', opts, attrs={'colspan': '-3'})
         colspan = opts.get_cell_colspan(tree_node)
         self.assertEqual(colspan, 1)
 
     def test_get_cell_rowspan(self):
         """ Test the ``get_cell_rowspan`` with a valid value.  """
         opts = TableCellTagOptions()
-        tree_node = TreeNode(None, 'td', opts, attrs={'rowspan': '3'})
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('td', opts, attrs={'rowspan': '3'})
         rowspan = opts.get_cell_rowspan(tree_node)
         self.assertEqual(rowspan, 3)
 
     def test_get_cell_rowspan_with_no_value_set(self):
         """ Test the ``get_cell_rowspan`` with no value set.  """
         opts = TableCellTagOptions()
-        tree_node = TreeNode(None, 'td', opts, attrs={})
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('td', opts, attrs={})
         rowspan = opts.get_cell_rowspan(tree_node)
         self.assertEqual(rowspan, 1)
 
     def test_get_cell_rowspan_with_non_number(self):
         """ Test the ``get_cell_rowspan`` with an invalid non-number value.  """
         opts = TableCellTagOptions()
-        tree_node = TreeNode(None, 'td', opts, attrs={'rowspan': 'abc'})
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('td', opts, attrs={'rowspan': 'abc'})
         rowspan = opts.get_cell_rowspan(tree_node)
         self.assertEqual(rowspan, 1)
 
     def test_get_cell_rowspan_with_negative_value(self):
         """ Test the ``get_cell_rowspan`` with a valid value.  """
         opts = TableCellTagOptions()
-        tree_node = TreeNode(None, 'td', opts, attrs={'rowspan': '-3'})
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('td', opts, attrs={'rowspan': '-3'})
         rowspan = opts.get_cell_rowspan(tree_node)
         self.assertEqual(rowspan, 1)
 
     def test_get_html_extra_attrs_blank(self):
         """ Test the ``get_html_extra_attrs`` method without any colspan or rowspan value set. """
         opts = TableCellTagOptions()
-        tree_node = TreeNode(None, 'td', opts, attrs={})
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('td', opts, attrs={})
         extra_attrs = opts.get_html_extra_attrs(tree_node)
         self.assertEqual('', extra_attrs)
 
     def test_get_html_extra_attrs_with_colspan(self):
         """ Test the ``get_html_extra_attrs`` method with colspan set. """
         opts = TableCellTagOptions()
-        tree_node = TreeNode(None, 'td', opts, attrs={'colspan': '3'})
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('td', opts, attrs={'colspan': '3'})
         extra_attrs = opts.get_html_extra_attrs(tree_node)
         self.assertEqual(' colspan="3"', extra_attrs)
 
     def test_get_html_extra_attrs_with_row_span(self):
         """ Test the ``get_html_extra_attrs`` method with rowspan set. """
         opts = TableCellTagOptions()
-        tree_node = TreeNode(None, 'td', opts, attrs={'rowspan': '3'})
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('td', opts, attrs={'rowspan': '3'})
         extra_attrs = opts.get_html_extra_attrs(tree_node)
         self.assertEqual(' rowspan="3"', extra_attrs)
 
     def test_get_html_extra_attrs_with_colspan_and_rowspan(self):
         """ Test the ``get_html_extra_attrs`` method with colspan and rowspan set. """
         opts = TableCellTagOptions()
-        tree_node = TreeNode(None, 'td', opts, attrs={'colspan': '2', 'rowspan': '3'})
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('td', opts, attrs={'colspan': '2', 'rowspan': '3'})
         extra_attrs = opts.get_html_extra_attrs(tree_node)
-        self.assertEqual(' colspan="2" rowspan="3"', extra_attrs)
-
-    def test_get_skcode_extra_attrs_blank(self):
-        """ Test the ``get_skcode_extra_attrs`` method without any colspan or rowspan value set. """
-        opts = TableCellTagOptions()
-        tree_node = TreeNode(None, 'td', opts, attrs={})
-        extra_attrs = opts.get_skcode_extra_attrs(tree_node)
-        self.assertEqual('', extra_attrs)
-
-    def test_get_skcode_extra_attrs_with_colspan(self):
-        """ Test the ``get_skcode_extra_attrs`` method with colspan set. """
-        opts = TableCellTagOptions()
-        tree_node = TreeNode(None, 'td', opts, attrs={'colspan': '3'})
-        extra_attrs = opts.get_skcode_extra_attrs(tree_node)
-        self.assertEqual(' colspan="3"', extra_attrs)
-
-    def test_get_skcode_extra_attrs_with_row_span(self):
-        """ Test the ``get_skcode_extra_attrs`` method with rowspan set. """
-        opts = TableCellTagOptions()
-        tree_node = TreeNode(None, 'td', opts, attrs={'rowspan': '3'})
-        extra_attrs = opts.get_skcode_extra_attrs(tree_node)
-        self.assertEqual(' rowspan="3"', extra_attrs)
-
-    def test_get_skcode_extra_attrs_with_colspan_and_rowspan(self):
-        """ Test the ``get_skcode_extra_attrs`` method with colspan and rowspan set. """
-        opts = TableCellTagOptions()
-        tree_node = TreeNode(None, 'td', opts, attrs={'colspan': '2', 'rowspan': '3'})
-        extra_attrs = opts.get_skcode_extra_attrs(tree_node)
         self.assertEqual(' colspan="2" rowspan="3"', extra_attrs)
 
     def test_html_rendering(self):
         """ Test HTML rendering. """
         opts = TableCellTagOptions()
-        tree_node = TreeNode(None, 'td', opts)
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('td', opts)
         rendered_output = opts.render_html(tree_node, 'test')
         expected_output = '<td>test</td>\n'
         self.assertEqual(expected_output, rendered_output)
@@ -247,7 +231,8 @@ class TableCellsTagtestCase(unittest.TestCase):
     def test_html_rendering_with_extra_attrs(self):
         """ Test HTML rendering with extra attributes. """
         opts = TableCellTagOptions()
-        tree_node = TreeNode(None, 'td', opts, attrs={'colspan': '2', 'rowspan': '3'})
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('td', opts, attrs={'colspan': '2', 'rowspan': '3'})
         rendered_output = opts.render_html(tree_node, 'test')
         expected_output = '<td colspan="2" rowspan="3">test</td>\n'
         self.assertEqual(expected_output, rendered_output)
@@ -255,7 +240,8 @@ class TableCellsTagtestCase(unittest.TestCase):
     def test_text_rendering(self):
         """ Test text rendering. """
         opts = TableCellTagOptions()
-        tree_node = TreeNode(None, 'td', opts)
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('td', opts)
         rendered_output = opts.render_text(tree_node, 'test')
         expected_output = 'test'
         self.assertEqual(expected_output, rendered_output)
@@ -263,30 +249,46 @@ class TableCellsTagtestCase(unittest.TestCase):
     def test_text_rendering_with_extra_attrs(self):
         """ Test text rendering with extra attributes. """
         opts = TableCellTagOptions()
-        tree_node = TreeNode(None, 'td', opts, attrs={'colspan': '2', 'rowspan': '3'})
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('td', opts, attrs={'colspan': '2', 'rowspan': '3'})
         rendered_output = opts.render_text(tree_node, 'test')
         expected_output = 'test'
         self.assertEqual(expected_output, rendered_output)
 
-    def test_skcode_rendering(self):
-        """ Test SkCode rendering. """
+    def test_get_skcode_attributes(self):
+        """ Test the ``get_skcode_attributes`` method. """
         opts = TableCellTagOptions()
-        tree_node = TreeNode(None, 'td', opts)
-        rendered_output = opts.render_skcode(tree_node, 'test')
-        expected_output = '[td]test[/td]'
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('td', opts)
+        rendered_output = opts.get_skcode_attributes(tree_node, 'test')
+        expected_output = ({'colspan': '', 'rowspan': ''}, None)
         self.assertEqual(expected_output, rendered_output)
 
-    def test_skcode_rendering_with_extra_attrs(self):
-        """ Test SkCode rendering with extra attributes. """
+    def test_get_skcode_attributes_with_extra_attrs(self):
+        """ Test the ``get_skcode_attributes`` method with extra attributes. """
         opts = TableCellTagOptions()
-        tree_node = TreeNode(None, 'td', opts, attrs={'colspan': '2', 'rowspan': '3'})
-        rendered_output = opts.render_skcode(tree_node, 'test')
-        expected_output = '[td colspan="2" rowspan="3"]test[/td]'
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('td', opts, attrs={'colspan': '1', 'rowspan': '1'})
+        rendered_output = opts.get_skcode_attributes(tree_node, 'test')
+        expected_output = ({'colspan': '', 'rowspan': ''}, None)
+        self.assertEqual(expected_output, rendered_output)
+
+    def test_get_skcode_attributes_with_extra_attrs_2(self):
+        """ Test the ``get_skcode_attributes`` method with extra attributes. """
+        opts = TableCellTagOptions()
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('td', opts, attrs={'colspan': '2', 'rowspan': '3'})
+        rendered_output = opts.get_skcode_attributes(tree_node, 'test')
+        expected_output = ({'colspan': '2', 'rowspan': '3'}, None)
         self.assertEqual(expected_output, rendered_output)
 
 
-class TableHeaderCellsTagtestCase(unittest.TestCase):
+class TableHeaderCellsTagTestCase(unittest.TestCase):
     """ Tests suite for the table cells tag module. """
+
+    def test_subclassing(self):
+        """ Test super class """
+        self.assertTrue(issubclass(TableHeaderCellTagOptions, TableCellTagOptions))
 
     def test_tag_and_aliases_in_default_recognized_tags_dict(self):
         """ Test the presence of the tag and aliases in the dictionary of default recognized tags. """
@@ -310,7 +312,8 @@ class TableHeaderCellsTagtestCase(unittest.TestCase):
     def test_html_rendering(self):
         """ Test HTML rendering. """
         opts = TableHeaderCellTagOptions()
-        tree_node = TreeNode(None, 'th', opts)
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('th', opts)
         rendered_output = opts.render_html(tree_node, 'test')
         expected_output = '<th>test</th>\n'
         self.assertEqual(expected_output, rendered_output)
@@ -318,7 +321,8 @@ class TableHeaderCellsTagtestCase(unittest.TestCase):
     def test_html_rendering_with_extra_attrs(self):
         """ Test HTML rendering with extra attributes. """
         opts = TableHeaderCellTagOptions()
-        tree_node = TreeNode(None, 'th', opts, attrs={'colspan': '2', 'rowspan': '3'})
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('th', opts, attrs={'colspan': '2', 'rowspan': '3'})
         rendered_output = opts.render_html(tree_node, 'test')
         expected_output = '<th colspan="2" rowspan="3">test</th>\n'
         self.assertEqual(expected_output, rendered_output)
@@ -326,7 +330,8 @@ class TableHeaderCellsTagtestCase(unittest.TestCase):
     def test_text_rendering(self):
         """ Test text rendering. """
         opts = TableHeaderCellTagOptions()
-        tree_node = TreeNode(None, 'th', opts)
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('th', opts)
         rendered_output = opts.render_text(tree_node, 'test')
         expected_output = 'test'
         self.assertEqual(expected_output, rendered_output)
@@ -334,23 +339,8 @@ class TableHeaderCellsTagtestCase(unittest.TestCase):
     def test_text_rendering_with_extra_attrs(self):
         """ Test text rendering with extra attributes. """
         opts = TableHeaderCellTagOptions()
-        tree_node = TreeNode(None, 'th', opts, attrs={'colspan': '2', 'rowspan': '3'})
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('th', opts, attrs={'colspan': '2', 'rowspan': '3'})
         rendered_output = opts.render_text(tree_node, 'test')
         expected_output = 'test'
-        self.assertEqual(expected_output, rendered_output)
-
-    def test_skcode_rendering(self):
-        """ Test SkCode rendering. """
-        opts = TableHeaderCellTagOptions()
-        tree_node = TreeNode(None, 'th', opts)
-        rendered_output = opts.render_skcode(tree_node, 'test')
-        expected_output = '[th]test[/th]'
-        self.assertEqual(expected_output, rendered_output)
-
-    def test_skcode_rendering_with_extra_attrs(self):
-        """ Test SkCode rendering with extra attributes. """
-        opts = TableHeaderCellTagOptions()
-        tree_node = TreeNode(None, 'th', opts, attrs={'colspan': '2', 'rowspan': '3'})
-        rendered_output = opts.render_skcode(tree_node, 'test')
-        expected_output = '[th colspan="2" rowspan="3"]test[/th]'
         self.assertEqual(expected_output, rendered_output)
