3a14ee8f28aed3245838c4fe9e5a5c3d40ae5981
Fabien Batteix <skywodd@gmail.com>
2016-02-03 15:40:26 +0100

    Updated tests suite

--- tests/tests_tags/tests_spoiler.py
+++ tests/tests_tags/tests_spoiler.py
@@ -4,12 +4,13 @@ SkCode spoiler tag test code.
 
 import unittest
 
-from skcode.etree import TreeNode
-from skcode.tags import (SpoilerTagOptions,
+from skcode.etree import RootTreeNode
+from skcode.tags import (RootTagOptions,
+                         SpoilerTagOptions,
                          DEFAULT_RECOGNIZED_TAGS)
 
 
-class HorizontalLineTagTestCase(unittest.TestCase):
+class SpoilerTagTestCase(unittest.TestCase):
     """ Tests suite for the spoiler tag module. """
 
     def test_tag_and_aliases_in_default_recognized_tags_dict(self):
@@ -30,19 +31,25 @@ class HorizontalLineTagTestCase(unittest.TestCase):
         self.assertFalse(opts.inline)
         self.assertTrue(opts.close_inlines)
         self.assertTrue(opts.make_paragraphs_here)
+        self.assertEqual('spoiler', opts.css_class_name)
 
     def test_render_html(self):
         """ Test the ``render_html`` method. """
         opts = SpoilerTagOptions()
-        self.assertEqual('<div class="spoiler">test</div>\n', opts.render_html(None, 'test'))
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('spoiler', opts)
+        self.assertEqual('<div class="spoiler">test</div>\n', opts.render_html(tree_node, 'test'))
+
+    def test_render_html_custom_css(self):
+        """ Test the ``render_html`` method with a custom CSS class name. """
+        opts = SpoilerTagOptions(css_class_name='custom_css')
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('spoiler', opts)
+        self.assertEqual('<div class="custom_css">test</div>\n', opts.render_html(tree_node, 'test'))
 
     def test_render_text(self):
         """ Test the ``render_text`` method. """
         opts = SpoilerTagOptions()
-        self.assertEqual('!!! SPOILER !!!\n! test\n!!!\n', opts.render_text(None, 'test'))
-
-    def test_render_skcode(self):
-        """ Test the ``render_skcode`` method. """
-        opts = SpoilerTagOptions()
-        tree_node = TreeNode(None, 'spoiler', opts)
-        self.assertEqual('[spoiler]test[/spoiler]', opts.render_skcode(tree_node, 'test'))
+        root_tree_node = RootTreeNode(RootTagOptions())
+        tree_node = root_tree_node.new_child('spoiler', opts)
+        self.assertEqual('!!! SPOILER !!!\n! test\n!!!\n', opts.render_text(tree_node, 'test'))
