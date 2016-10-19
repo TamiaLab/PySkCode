ba71a981042e51e9bbf0531bb1cadd1803565e9d
Fabien Batteix <skywodd@gmail.com>
2016-10-19 21:31:53 +0200

    Tests fixed

--- tests/tests_tags/tests_webspecials.py
+++ tests/tests_tags/tests_webspecials.py
@@ -4,44 +4,45 @@ SkCode web special tag definitions test code.
 
 import unittest
 
-from skcode.tags import (HorizontalLineTagOptions,
-                         LineBreakTagOptions,
-                         CutHereTagOptions,
-                         DEFAULT_RECOGNIZED_TAGS)
-from skcode.tags.webspecials import CUT_HERE_STRING
+from skcode.etree import RootTreeNode
+from skcode.tags import (
+    HorizontalLineTreeNode,
+    LineBreakTreeNode,
+    CutHereTreeNode,
+    DEFAULT_RECOGNIZED_TAGS_LIST
+)
 
 
-class HorizontalLineTagOptionsTestCase(unittest.TestCase):
+class HorizontalLineTreeNodeTestCase(unittest.TestCase):
     """ Tests suite for the horizontal line tag module. """
 
     def test_tag_and_aliases_in_default_recognized_tags_dict(self):
         """ Test the presence of the tag and aliases in the dictionary of default recognized tags. """
-        self.assertIn('hr', DEFAULT_RECOGNIZED_TAGS)
-        self.assertIsInstance(DEFAULT_RECOGNIZED_TAGS['hr'], HorizontalLineTagOptions)
+        self.assertIn(HorizontalLineTreeNode, DEFAULT_RECOGNIZED_TAGS_LIST)
 
     def test_tag_constant_values(self):
         """ Test tag constants. """
-        opts = HorizontalLineTagOptions()
-        self.assertFalse(opts.newline_closes)
-        self.assertFalse(opts.same_tag_closes)
-        self.assertTrue(opts.standalone)
-        self.assertTrue(opts.parse_embedded)
-        self.assertFalse(opts.swallow_trailing_newline)
-        self.assertFalse(opts.inline)
-        self.assertTrue(opts.close_inlines)
-        self.assertEqual('hr', opts.canonical_tag_name)
-        self.assertEqual((), opts.alias_tag_names)
-        self.assertFalse(opts.make_paragraphs_here)
+        self.assertFalse(HorizontalLineTreeNode.newline_closes)
+        self.assertFalse(HorizontalLineTreeNode.same_tag_closes)
+        self.assertTrue(HorizontalLineTreeNode.standalone)
+        self.assertTrue(HorizontalLineTreeNode.parse_embedded)
+        self.assertFalse(HorizontalLineTreeNode.inline)
+        self.assertTrue(HorizontalLineTreeNode.close_inlines)
+        self.assertEqual('hr', HorizontalLineTreeNode.canonical_tag_name)
+        self.assertEqual((), HorizontalLineTreeNode.alias_tag_names)
+        self.assertFalse(HorizontalLineTreeNode.make_paragraphs_here)
 
     def test_render_html(self):
         """ Test the ``render_html`` method. """
-        opts = HorizontalLineTagOptions()
-        self.assertEqual('<hr>\n', opts.render_html(None, ''))
+        root_tree_node = RootTreeNode()
+        tree_node = root_tree_node.new_child('hr', HorizontalLineTreeNode)
+        self.assertEqual('<hr>\n', tree_node.render_html(''))
 
     def test_render_text(self):
         """ Test the ``render_text`` method. """
-        opts = HorizontalLineTagOptions()
-        self.assertEqual('----------\n', opts.render_text(None, ''))
+        root_tree_node = RootTreeNode()
+        tree_node = root_tree_node.new_child('hr', HorizontalLineTreeNode)
+        self.assertEqual('----------\n', tree_node.render_text(''))
 
 
 class LineBreakTagTestCase(unittest.TestCase):
@@ -49,66 +50,61 @@ class LineBreakTagTestCase(unittest.TestCase):
 
     def test_tag_and_aliases_in_default_recognized_tags_dict(self):
         """ Test the presence of the tag and aliases in the dictionary of default recognized tags. """
-        self.assertIn('br', DEFAULT_RECOGNIZED_TAGS)
-        self.assertIsInstance(DEFAULT_RECOGNIZED_TAGS['br'], LineBreakTagOptions)
+        self.assertIn(LineBreakTreeNode, DEFAULT_RECOGNIZED_TAGS_LIST)
 
     def test_tag_constant_values(self):
         """ Test tag constants. """
-        opts = LineBreakTagOptions()
-        self.assertFalse(opts.newline_closes)
-        self.assertFalse(opts.same_tag_closes)
-        self.assertTrue(opts.standalone)
-        self.assertTrue(opts.parse_embedded)
-        self.assertFalse(opts.swallow_trailing_newline)
-        self.assertTrue(opts.inline)
-        self.assertFalse(opts.close_inlines)
-        self.assertEqual('br', opts.canonical_tag_name)
-        self.assertEqual((), opts.alias_tag_names)
-        self.assertFalse(opts.make_paragraphs_here)
+        self.assertFalse(LineBreakTreeNode.newline_closes)
+        self.assertFalse(LineBreakTreeNode.same_tag_closes)
+        self.assertTrue(LineBreakTreeNode.standalone)
+        self.assertTrue(LineBreakTreeNode.parse_embedded)
+        self.assertTrue(LineBreakTreeNode.inline)
+        self.assertFalse(LineBreakTreeNode.close_inlines)
+        self.assertEqual('br', LineBreakTreeNode.canonical_tag_name)
+        self.assertEqual((), LineBreakTreeNode.alias_tag_names)
+        self.assertFalse(LineBreakTreeNode.make_paragraphs_here)
 
     def test_render_html(self):
         """ Test the ``render_html`` method. """
-        opts = LineBreakTagOptions()
-        self.assertEqual('<br>\n', opts.render_html(None, ''))
+        root_tree_node = RootTreeNode()
+        tree_node = root_tree_node.new_child('br', LineBreakTreeNode)
+        self.assertEqual('<br>\n', tree_node.render_html(''))
 
     def test_render_text(self):
         """ Test the ``render_text`` method. """
-        opts = LineBreakTagOptions()
-        self.assertEqual('\n', opts.render_text(None, ''))
+        root_tree_node = RootTreeNode()
+        tree_node = root_tree_node.new_child('br', LineBreakTreeNode)
+        self.assertEqual('\n', tree_node.render_text(''))
 
 
-class CutHereTagOptionsTestCase(unittest.TestCase):
+class CutHereTreeNodeTestCase(unittest.TestCase):
     """ Tests suite for the "cut here" tag module. """
 
     def test_tag_and_aliases_in_default_recognized_tags_dict(self):
         """ Test the presence of the tag and aliases in the dictionary of default recognized tags. """
-        self.assertIn('cuthere', DEFAULT_RECOGNIZED_TAGS)
-        self.assertIsInstance(DEFAULT_RECOGNIZED_TAGS['cuthere'], CutHereTagOptions)
-
-    def test_module_constants(self):
-        """ Test module level costants """
-        self.assertEqual('<!-- Cut Here -->', CUT_HERE_STRING)
+        self.assertIn(CutHereTreeNode, DEFAULT_RECOGNIZED_TAGS_LIST)
 
     def test_tag_constant_values(self):
         """ Test tag constants. """
-        opts = CutHereTagOptions()
-        self.assertFalse(opts.newline_closes)
-        self.assertFalse(opts.same_tag_closes)
-        self.assertTrue(opts.standalone)
-        self.assertTrue(opts.parse_embedded)
-        self.assertFalse(opts.swallow_trailing_newline)
-        self.assertFalse(opts.inline)
-        self.assertTrue(opts.close_inlines)
-        self.assertEqual('cuthere', opts.canonical_tag_name)
-        self.assertEqual((), opts.alias_tag_names)
-        self.assertFalse(opts.make_paragraphs_here)
+        self.assertFalse(CutHereTreeNode.newline_closes)
+        self.assertFalse(CutHereTreeNode.same_tag_closes)
+        self.assertTrue(CutHereTreeNode.standalone)
+        self.assertTrue(CutHereTreeNode.parse_embedded)
+        self.assertFalse(CutHereTreeNode.inline)
+        self.assertTrue(CutHereTreeNode.close_inlines)
+        self.assertEqual('cuthere', CutHereTreeNode.canonical_tag_name)
+        self.assertEqual((), CutHereTreeNode.alias_tag_names)
+        self.assertFalse(CutHereTreeNode.make_paragraphs_here)
+        self.assertEqual('<!-- Cut Here -->', CutHereTreeNode.delimiter_string_html)
 
     def test_render_html(self):
         """ Test the ``render_html`` method. """
-        opts = CutHereTagOptions()
-        self.assertEqual('<!-- Cut Here -->\n', opts.render_html(None, ''))
+        root_tree_node = RootTreeNode()
+        tree_node = root_tree_node.new_child('cuthere', CutHereTreeNode)
+        self.assertEqual('\n<!-- Cut Here -->\n', tree_node.render_html(''))
 
     def test_render_text(self):
         """ Test the ``render_text`` method. """
-        opts = CutHereTagOptions()
-        self.assertEqual('', opts.render_text(None, ''))
+        root_tree_node = RootTreeNode()
+        tree_node = root_tree_node.new_child('cuthere', CutHereTreeNode)
+        self.assertEqual('', tree_node.render_text(''))
