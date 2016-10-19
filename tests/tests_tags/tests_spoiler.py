b01bdcd8e151de42bfd787864bb2684cb10af2d3
Fabien Batteix <skywodd@gmail.com>
2016-10-19 21:08:28 +0200

    Tests fixed

--- tests/tests_tags/tests_spoiler.py
+++ tests/tests_tags/tests_spoiler.py
@@ -5,9 +5,15 @@ SkCode spoiler tag definitions test code.
 import unittest
 
 from skcode.etree import RootTreeNode
-from skcode.tags import (RootTagOptions,
-                         SpoilerTagOptions,
-                         DEFAULT_RECOGNIZED_TAGS)
+from skcode.tags import (
+    SpoilerTreeNode,
+    DEFAULT_RECOGNIZED_TAGS_LIST
+)
+
+
+class CustomSpoilerTreeNode(SpoilerTreeNode):
+    """ Custom spoiler class """
+    css_class_name = 'custom_css'
 
 
 class SpoilerTagTestCase(unittest.TestCase):
@@ -15,44 +21,36 @@ class SpoilerTagTestCase(unittest.TestCase):
 
     def test_tag_and_aliases_in_default_recognized_tags_dict(self):
         """ Test the presence of the tag and aliases in the dictionary of default recognized tags. """
-        self.assertIn('spoiler', DEFAULT_RECOGNIZED_TAGS)
-        self.assertIsInstance(DEFAULT_RECOGNIZED_TAGS['spoiler'], SpoilerTagOptions)
-        self.assertIn('hide', DEFAULT_RECOGNIZED_TAGS)
-        self.assertIsInstance(DEFAULT_RECOGNIZED_TAGS['hide'], SpoilerTagOptions)
+        self.assertIn(SpoilerTreeNode, DEFAULT_RECOGNIZED_TAGS_LIST)
 
     def test_tag_constant_values(self):
         """ Test tag constants. """
-        opts = SpoilerTagOptions()
-        self.assertFalse(opts.newline_closes)
-        self.assertFalse(opts.same_tag_closes)
-        self.assertFalse(opts.standalone)
-        self.assertTrue(opts.parse_embedded)
-        self.assertFalse(opts.swallow_trailing_newline)
-        self.assertFalse(opts.inline)
-        self.assertTrue(opts.close_inlines)
-        self.assertEqual('spoiler', opts.canonical_tag_name)
-        self.assertEqual(('hide', ), opts.alias_tag_names)
-        self.assertTrue(opts.make_paragraphs_here)
-        self.assertEqual('spoiler', opts.css_class_name)
-        self.assertEqual('<div class="{class_name}">{inner_html}</div>\n', opts.html_render_template)
+        self.assertFalse(SpoilerTreeNode.newline_closes)
+        self.assertFalse(SpoilerTreeNode.same_tag_closes)
+        self.assertFalse(SpoilerTreeNode.standalone)
+        self.assertTrue(SpoilerTreeNode.parse_embedded)
+        self.assertFalse(SpoilerTreeNode.inline)
+        self.assertTrue(SpoilerTreeNode.close_inlines)
+        self.assertEqual('spoiler', SpoilerTreeNode.canonical_tag_name)
+        self.assertEqual(('hide', ), SpoilerTreeNode.alias_tag_names)
+        self.assertTrue(SpoilerTreeNode.make_paragraphs_here)
+        self.assertEqual('spoiler', SpoilerTreeNode.css_class_name)
+        self.assertEqual('<div class="{class_name}">{inner_html}</div>\n', SpoilerTreeNode.html_render_template)
 
     def test_render_html(self):
         """ Test the ``render_html`` method. """
-        opts = SpoilerTagOptions()
-        root_tree_node = RootTreeNode(RootTagOptions())
-        tree_node = root_tree_node.new_child('spoiler', opts)
-        self.assertEqual('<div class="spoiler">test</div>\n', opts.render_html(tree_node, 'test'))
+        root_tree_node = RootTreeNode()
+        tree_node = root_tree_node.new_child('spoiler', SpoilerTreeNode)
+        self.assertEqual('<div class="spoiler">test</div>\n', tree_node.render_html('test'))
 
     def test_render_html_custom_css(self):
         """ Test the ``render_html`` method with a custom CSS class name. """
-        opts = SpoilerTagOptions(css_class_name='custom_css')
-        root_tree_node = RootTreeNode(RootTagOptions())
-        tree_node = root_tree_node.new_child('spoiler', opts)
-        self.assertEqual('<div class="custom_css">test</div>\n', opts.render_html(tree_node, 'test'))
+        root_tree_node = RootTreeNode()
+        tree_node = root_tree_node.new_child('spoiler', CustomSpoilerTreeNode)
+        self.assertEqual('<div class="custom_css">test</div>\n', tree_node.render_html('test'))
 
     def test_render_text(self):
         """ Test the ``render_text`` method. """
-        opts = SpoilerTagOptions()
-        root_tree_node = RootTreeNode(RootTagOptions())
-        tree_node = root_tree_node.new_child('spoiler', opts)
-        self.assertEqual('!!! SPOILER !!!\n! test\n!!!\n', opts.render_text(tree_node, 'test'))
+        root_tree_node = RootTreeNode()
+        tree_node = root_tree_node.new_child('spoiler', SpoilerTreeNode)
+        self.assertEqual('!!! SPOILER !!!\n! test\n!!!\n', tree_node.render_text('test'))
